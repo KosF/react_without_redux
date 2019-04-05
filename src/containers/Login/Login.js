50da7f4a73f6f49ebf08e653380f2956d439971e
Kos <kostiantyn.filipenkov@aroundhome.de>
2019-04-05 13:55:01 +0300

    added hooks

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
@@ -7,36 +7,28 @@ import { logIn, logOut } from "Src/store/Auth/authActions";
 import { setCookie } from "Src/helpers/cookie";
 import checkCredentials from "Src/helpers/checkCredentials";
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
+function Login({ ...props }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMsg, setErrorMsg] = useState(false);
 
-    this.state = {
-      username: "",
-      password: "",
-      isAuth: false,
-      errorMsg: false
-    };
-
-    this.handleChangeInput = this.handleChangeInput.bind(this);
-    this.handleSubmitForm = this.handleSubmitForm.bind(this);
-  }
-
-  handleChangeInput(e) {
-    const value = e.currentTarget.value.toString();
+  const handleChangeInput = e => {
+    const value = e.currentTarget.value.toString().trim();
     const fieldName = e.currentTarget.dataset.inputName;
 
-    this.setState(prevState => ({
-      ...prevState,
-      errorMsg: false,
-      [fieldName]: value
-    }));
-  }
+    setErrorMsg(false);
 
-  handleSubmitForm(e) {
+    if (fieldName === "username") {
+      setUsername(value);
+    } else if (fieldName === "password") {
+      setPassword(value);
+    }
+  };
+
+  const handleSubmitForm = e => {
     e.preventDefault();
-    const { history, location } = this.props;
-    const { username, password } = this.state;
+    const { history, location } = props;
+
     const validCredentials = checkCredentials({
       username,
       password
@@ -45,74 +37,66 @@ class Login extends Component {
     const nextLocation = location.prevLocation ? location.prevLocation : "/";
 
     if (!validCredentials) {
-      this.setState({ errorMsg: true });
+      setErrorMsg("true");
     } else {
       setCookie("isAuth", true);
 
-      this.props.logIn({ username, password }, () =>
-        history.push(nextLocation)
-      );
+      props.logIn({ username, password }, () => history.push(nextLocation));
     }
-  }
+  };
 
-  render() {
-    return (
-      <div className="d-flex justify-content-center">
-        <form
-          className="col-6 pt-4 pr-3 pb-4 pl-3 bg-light"
-          onSubmit={this.handleSubmitForm}
-        >
-          <fieldset>
+  return (
+    <div className="d-flex justify-content-center">
+      <form
+        className="col-6 pt-4 pr-3 pb-4 pl-3 bg-light"
+        onSubmit={handleSubmitForm}
+      >
+        <fieldset>
+          <div className="row mb-3">
+            <div className="col">
+              <input
+                type="text"
+                data-input-name="username"
+                className="form-control"
+                placeholder="Name"
+                onChange={handleChangeInput}
+              />
+            </div>
+          </div>
+          <div className="row mb-3">
+            <div className="col">
+              <input
+                type="text"
+                data-input-name="password"
+                className="form-control"
+                placeholder="Password"
+                onChange={handleChangeInput}
+              />
+            </div>
+          </div>
+          {errorMsg && (
             <div className="row mb-3">
               <div className="col">
-                <input
-                  type="text"
-                  data-input-name="username"
-                  className="form-control"
-                  placeholder="Name"
-                  onChange={this.handleChangeInput}
-                />
+                <p className="text-danger mb-0">
+                  Имя пользователя или пароль введены не верно
+                </p>
               </div>
             </div>
-            <div className="row mb-3">
-              <div className="col">
-                <input
-                  type="text"
-                  data-input-name="password"
-                  className="form-control"
-                  placeholder="Password"
-                  onChange={this.handleChangeInput}
-                />
-              </div>
+          )}
+          <div className="row">
+            <div className="col">
+              <input
+                className="btn btn-primary w-100"
+                type="submit"
+                value="Login"
+              />
             </div>
-            {this.state.errorMsg && (
-              <div className="row mb-3">
-                <div className="col">
-                  <p className="text-danger mb-0">
-                    Имя пользователя или пароль введены не верно
-                  </p>
-                </div>
-              </div>
-            )}
-            <div className="row">
-              <div className="col">
-                <input
-                  className="btn btn-primary w-100"
-                  type="submit"
-                  value="Login"
-                />
-              </div>
-            </div>
-          </fieldset>
-        </form>
-      </div>
-    );
-  }
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  );
 }
-
-const mapStateToProps = store => ({
-  isAuth: store.authReducer.isAuth
-});
 
 const mapDispatchToProps = { logIn, logOut };
 
@@ -126,7 +110,7 @@ Login.propTypes = {
 
 export default withRouter(
   connect(
-    mapStateToProps,
+    null,
     mapDispatchToProps
   )(Login)
 );
